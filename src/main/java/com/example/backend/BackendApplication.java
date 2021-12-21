c313b5f5f16aa54528dab053274bfb54be25308f
maltinisik <metin.altinisik@gmail.com>
2021-12-21 15:45:48 +0300

    Update BackendApplication.java

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		/* comment added */
 		SpringApplication.run(BackendApplication.class, args);
 	}
 }
